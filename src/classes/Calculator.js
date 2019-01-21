2737b5c4dc15a7939b11d0c995eb110c75befda5
thienJs <cristian.albarran95@gmail.com>
2019-01-21 17:46:04 -0400

    refactoring Calculator Class

--- src/classes/Calculator.js
+++ src/classes/Calculator.js
@@ -1,25 +1,15 @@
-export const Calculator = (() => {
-  class Calculator {
-    _result = 0;
+class Calculator {
+  _result = 0;
 
-    getResult = () => this._result;
+  getResult = () => this._result;
 
-    sum = num => this._result + +num;
+  sum = num => this._result + +num;
 
-    subs = num => this._result - +num;
+  subs = num => this._result - +num;
 
-    multiply = num => this._result * +num;
+  multiply = num => this._result * +num;
 
-    divide = num => this._result / +num;
-  }
+  divide = num => this._result / +num;
+}
 
-  const { getResult, sum, subs, multiply, divide } = new Calculator();
-
-  return {
-    getResult,
-    sum,
-    subs,
-    multiply,
-    divide
-  };
-})();
+export const { getResult, sum, subs, multiply, divide } = new Calculator();
